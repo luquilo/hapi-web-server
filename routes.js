2597e4df6fdff2809a315302fd21c03ac46ff7c7
luquilo <luquilo.etc@gmail.com>
2023-01-06 06:45:42 +0700

    menambahkan beberapa route

--- routes.js
+++ routes.js
@@ -3,16 +3,31 @@ const routes = [
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "ini homepage";
+      return "return homepage";
     },
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+        return `tidak dapat mengakses halaman dengan method ${method}`
+    }
   },
   {
     method: "GET",
     path: "/about",
     handler: (request, h) => {
-      return "ini adalah halaman about";
+      return "return about page";
     },
   },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+        return `tidak dapat mengakses halaman dengan method ${method}`
+    }
+  }
+  
 ];
 
 module.exports = routes
