99ae3fabc0cf5889f2fe8b6182bf7f8f6816f9b7
luquilo <luquilo.etc@gmail.com>
2023-01-05 23:35:11 +0700

    lanjut besok

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log('hao');
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost'
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'hello world'
+        }
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/ho',
+        handler: (request, h) => {
+            return 'hello world setengah'
+        }
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/home',
+        handler: (request, h) => {
+            return 'hello world lengkap'
+        }
+    })
+
+
+    await server.start()
+    console.log(`server berjalan pada ${server.info.uri}`)
+}
+
+init()
